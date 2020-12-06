3b08f67b84af51288299586e40d9dde326aea30b
Maxi Burastero <maxicc4@gmail.com>
2020-12-06 20:53:47 -0300

    Cabina con curvas de Bezier

--- tp1/js/object3d.js
+++ tp1/js/object3d.js
@@ -25,22 +25,22 @@ class Object3D {
 class Helicopter extends Object3D{
     constructor(m) {
         let children = [];
-        children.push(new Sphere(m));
-        children.push(new Sphere(m));
+        //children.push(new Sphere(m));
+        //children.push(new Sphere(m));
         super(m, new Grilla3D(new Cabina(1.25, 0.6, 0.25)), children);
         this.setM(m);
     }
 
     setM(m) {
         super.setM(m);
-        let m1 = mat4.create();
+        /*let m1 = mat4.create();
         mat4.translate(m1, m, [0,0.1,0.25]);
         mat4.scale(m1, m1, [0.25,0.25,0.25]);
         this.children[0].setM(m1);
         let m2 = mat4.create();
         mat4.translate(m2, m, [0,0.1,-0.25]);
         mat4.scale(m2, m2, [0.25,0.25,0.25]);
-        this.children[1].setM(m2);
+        this.children[1].setM(m2);*/
     }
 }
 
